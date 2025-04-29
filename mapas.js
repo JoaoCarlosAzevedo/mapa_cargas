ed3d5f1b6bbe7effa9db4ac6e63c5bee62a02dd8
Joao Carlos <jcorreadeazevedo@gmail.com>
2025-04-29 08:26:12 -0400

    ajustes legendas

--- mapas.js
+++ mapas.js
@@ -22,14 +22,26 @@ twebchannel.advplToJs = function (key, value) {
 
 // Ícones personalizados para os marcadores
 var customIcons = {
-  default: L.icon({
+  red: L.icon({
     iconUrl: "https://maps.google.com/mapfiles/ms/icons/red-dot.png",
     iconSize: [32, 32],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32],
   }),
-  changed: L.icon({
+  green: L.icon({
     iconUrl: "https://maps.google.com/mapfiles/ms/icons/green-dot.png",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  }),
+  yellow: L.icon({
+    iconUrl: "https://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  }),
+  blue: L.icon({
+    iconUrl: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
     iconSize: [32, 32],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32],
@@ -68,24 +80,16 @@ function refreshLocations(locationsString) {
 
   locations.forEach((location) => {
     let marker = L.marker([location.lat, location.lng], {
-      icon: location.marcado ? customIcons.changed : customIcons.default,
+    //icon: location.marcado ? customIcons.changed : customIcons.default,
+    icon: customIcons[location.legenda]
     });
 
- /*    let popupContent = `
-            <h3>${location.UF} - ${location.CIDADE}</h3>
-            <b>${location.title}</b> 
-            <br><b>Pedido:</b> ${location.ID}
-            <br><b>Produto:</b> ${location.produto}
-            <br><b>Entrega:</b> ${location.entrega}
-            <br><b>Quant.:</b> ${location.quantidade.toFixed(0)}
-            <br><b>Obs:</b> ${location.obs}
-            <br><b>Veículo:</b> ${location.placa}
-        `; */
       let popupContent = `
         <div style="font-size: 14px; line-height: 1.4; word-break: break-word;">
         <h3 style="margin: 0;">${location.UF} - ${location.CIDADE}</h3>
         <b>${location.title}</b> 
         <div><b>Pedido:</b> ${location.ID}</div>
+        <div><b>Status Ped.:</b> ${location.status}</div>
         <div><b>Produto:</b> ${location.produto}</div>
         <div><b>Entrega:</b> ${location.entrega}</div>
         <div><b>Quant.:</b> ${location.quantidade.toFixed(0)}</div>
@@ -105,50 +109,4 @@ function refreshLocations(locationsString) {
   }); 
 }
 
-// Função para carregar locais   do JSON
-//function loadLocations() {
-//    fetch('locations.json')
-//        .then(response => response.json())
-//        .then(locations => {
-//            locations.forEach(location => {
-//                let marker = L.marker([location.lat, location.lng], { icon: customIcons.default })
-//                    .addTo(map)
-//                /*     .bindPopup(`
-//                        <h3>${location.UF} - ${location.CIDADE}</h3>
-//                        <b>${location.title}</b>
-//                        <br>
-//                        <b>Pedido ${location.ID}</b>
-//                        `); */
-//
-//                let popupContent = `
-//                        <h3>${location.UF} - ${location.CIDADE}</h3>
-//                        <b>${location.title}</b>
-//                        <br>
-//                        <b>Pedido ${location.ID}</b>
-//                        `;
-//                // Exibir popup ao passar o mouse
-//                marker.on('mouseover', function () {
-//                    marker.bindPopup(popupContent).openPopup();
-//                });
-//
-//                // Fechar popup ao tirar o mouse
-//                marker.on('mouseout', function () {
-//                    marker.closePopup();
-//                });
-//
-//
-//                // Evento de clique no marcador para exibir no console
-//                   marker.on('click', function () {
-//
-//                       twebchannel.jsToAdvpl("MARK", location.ID);
-//                       //let currentIcon = marker.options.icon;
-//                        //marker.setIcon(currentIcon === customIcons.default ? customIcons.changed : customIcons.default);
-//
-//                   });
-//                   //twebchannel.jsToAdvpl("KEY", "YAHOOOO!");
-//            });
-//        })
-//        .catch(error => console.error("Erro ao carregar locais:", error));
-//};
-//
-//loadLocations(); //
+
